a8363aa225c79913607a15a37ca80cdbca810766
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2026-06-02 17:24:45 +0100

    feat:added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AnalyticsProvider from "./components/AnalyticsProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,7 +46,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    > <AnalyticsProvider/>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
